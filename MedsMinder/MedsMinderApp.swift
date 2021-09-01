17b289ee1cc0ad7137119b01bdddcd3a7120ce91
ReneeBe <reneelberger@gmail.com>
2021-09-01 08:50:04 -0700

    Initial Commit

---
+++ MedsMinder/MedsMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsMinderApp.swift
+//  MedsMinder
+//
+//  Created by Renee Berger on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MedsMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
